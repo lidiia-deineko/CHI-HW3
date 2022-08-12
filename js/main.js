053138ecf66b63574d4287fac26ef3500236950b
Lida Deineko <93deineko@gmail.com>
2022-08-12 19:06:47 +0300

    Add events section with 2 sliders (without full responsive)

--- js/main.js
+++ js/main.js
@@ -48,6 +48,73 @@ window.addEventListener('load', () => {
 
 
    
+      //slider in events section
+
+      $(".events-content").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        arrows: false,
+        fade: true,
+        asNavFor: ".events-name"
+      });
+      $(".events-name").slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        speed: 500,
+        asNavFor: ".events-content",
+        dots: false,
+        centerMode: false,
+        focusOnSelect: true,
+        slide: "div",
+        prevArrow: '<button type="button" class="btn-prev"><img src="images/prev-slider-top.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="btn-next"><img src="images/next-clider-top.svg" alt=""></button>',
+        responsive: [
+            {
+              breakpoint: 1900,
+              settings: {
+                slidesToShow: 4,
+                arrows: true
+              }
+            },
+            {
+                breakpoint: 1560,
+                settings: {
+                  slidesToShow: 3,
+                  arrows: true
+                }
+              },
+              {
+                breakpoint: 1200,
+                settings: {
+                  slidesToShow: 2,
+                  arrows: true
+                }
+              },
+              {
+                breakpoint: 750,
+                settings: {
+                  slidesToShow: 1,
+                  arrows: false,
+                  centerMode: true,
+                }
+              }
+          ]
+      });
+      
+
+      //slider inside event section
+ 
+      $(".item-slider__wrapper").slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<button type="button" class="events-btn-prev"><img src="images/prev-slider-top.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="events-btn-next"><img src="images/next-clider-top.svg" alt=""></button>',
+      });
       
 })
 
